8680d689adf7389d5641629ce545b7125e662fda
Martha Correa <165719743+mtcorread@users.noreply.github.com>
2025-05-11 22:25:43 +0100

    Initial commit (clean version)

--- hook_imports.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Add your project's root directory to the path
-module_dir = os.path.dirname(sys.modules['__main__'].__file__)
-if module_dir not in sys.path:
-    sys.path.insert(0, module_dir)
